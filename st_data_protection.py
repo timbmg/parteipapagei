2234583c7cca6844f2b61c874f640481c3b4a15a
Tim Baumgärtner <baumgaertner.t@gmail.com>
2025-01-14 17:02:22 +0100

    insert data_protenction.md

--- st_data_protection.py
+++ st_data_protection.py
@@ -6,6 +6,5 @@ cookie_controller = CookieController()
 
 pseudo_user_id = cookie_controller.get("pseudo-user-id")
 
-st.title("🔒 Datenschutz")
-
-st.markdown(f"""Deine Pseudo-User-ID: `{pseudo_user_id}`""")
+with open("data_protection.md") as md_file:
+    st.markdown(md_file.read().format(pseudo_user_id=pseudo_user_id))
